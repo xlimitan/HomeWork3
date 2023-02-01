724d9347f4d18209d50349bad85248f01200c9f2
xlimitan <xlimitan@yandex.ru>
2023-02-01 16:31:12 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
 
     }
 
@@ -30,5 +32,28 @@ public class Main {
         short six = 27897;
         byte seven = 67;
     }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        byte ludmilaPavlovna = 23;
+        byte annaSergeevna = 27;
+        byte ekaterinaAndreevna = 30;
+        short paper = 480;
+        byte allStudent = (byte) (ludmilaPavlovna + annaSergeevna + ekaterinaAndreevna);
+        short paperOneStudent = (short) (paper / allStudent);
+        System.out.println("На каждого ученика рассчитано " + paperOneStudent + " листов бумаги");
 
-}
+        }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        byte efficiency = 16;
+        byte twoMinutes = 2;
+        byte twentyMinutes = 20;
+        short day = 1440;
+        short threeDay = (short) (day * 3);
+        int year = (int) (day * 365);
+        System.out.println("За 20 минут машина произвела " + (twentyMinutes / twoMinutes * efficiency) + " штук бутылок");
+        System.out.println("За сутки машина произвела " + (day / twoMinutes * efficiency) + " штук бутылок");
+        System.out.println("За три дня машина произвела " + (threeDay / twoMinutes * efficiency) + " штук бутылок");
+        System.out.println("За год машина произвела " + (year / twoMinutes * efficiency) + " штук бутылок");
+    }
+    }
