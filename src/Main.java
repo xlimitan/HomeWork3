e4e6f98e347df9803951ced3ffa5a8e7ecd34dee
xlimitan <xlimitan@yandex.ru>
2023-02-01 15:40:28 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte one = 1;
+        short two = 2;
+        int three = 3;
+        long four = 4;
+        float five = 5f;
+        double six = 6d;
+        System.out.println("Значение переменной one с типом byte равно " + one);
+        }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        byte one = 1;
+        short two = 2;
+        int three = 3;
+        long four = 4;
+        float five = 5f;
+        double six = 6d;
+        System.out.println("Значение переменной one с типом byte равно " + one);
+    }
+
+}
