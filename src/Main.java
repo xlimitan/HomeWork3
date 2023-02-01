5298e3e543ae16ddc1f40502342df6433402d44e
xlimitan <xlimitan@yandex.ru>
2023-02-01 15:44:54 +0300

    Задача 1.2

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,15 @@ public class Main {
         float five = 5f;
         double six = 6d;
         System.out.println("Значение переменной one с типом byte равно " + one);
+        System.out.println("Значение переменной two с типом byte равно " + two);
+        System.out.println("Значение переменной three с типом byte равно " + three);
+        System.out.println("Значение переменной four с типом byte равно " + four);
+        System.out.println("Значение переменной five с типом byte равно " + five);
+        System.out.println("Значение переменной six с типом byte равно " + six);
         }
     public static void task2 () {
         System.out.println("Задача 2");
-        byte one = 1;
-        short two = 2;
-        int three = 3;
-        long four = 4;
-        float five = 5f;
-        double six = 6d;
-        System.out.println("Значение переменной one с типом byte равно " + one);
+        
     }
 
 }
